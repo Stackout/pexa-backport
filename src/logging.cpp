5c82c3c0a466acd86922ffcc4eb6cb1552b80f3d
Ryan Hein <me@ryanhein.com>
2020-05-31 14:43:23 -0600

    Add initial support for veriblock.

--- src/logging.cpp
+++ src/logging.cpp
@@ -162,7 +162,7 @@ const CLogCategoryDesc LogCategories[] =
     {BCLog::COINDB, "coindb"},
     {BCLog::QT, "qt"},
     {BCLog::LEVELDB, "leveldb"},
-    {BCLog::VALIDATION, "validation"},
+    {BCLog::POP, "pop"},
     {BCLog::ALL, "1"},
     {BCLog::ALL, "all"},
 };
--- src/logging.h
+++ src/logging.h
@@ -55,7 +55,7 @@ namespace BCLog {
         COINDB      = (1 << 18),
         QT          = (1 << 19),
         LEVELDB     = (1 << 20),
-        VALIDATION  = (1 << 21),
+        POP         = (1 << 21),
         ALL         = ~(uint32_t)0,
     };
 
